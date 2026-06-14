3ec3f9cda46c86691921e294b486da44e3a7ac6b
Iris Shi <shi.iris0505@gmail.com>
2026-06-14 00:28:47 -0400

    gera

--- frontend/src/components/icons.tsx
+++ frontend/src/components/icons.tsx
@@ -42,7 +42,7 @@ export function LockIcon(props: SVGProps<SVGSVGElement>) {
 
 export function GearIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 310.34 305.05" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg viewBox="0 10 310.34 305.05" xmlns="http://www.w3.org/2000/svg" {...props}>
       <circle cx="155.17" cy="152.53" r="123.5" />
       <rect x="-1.83" y="125.03" width="314" height="55" rx="7.33" ry="7.33" transform="translate(83.14 -52.79) rotate(26.16)" />
       <rect x="-1.83" y="125.03" width="314" height="55" rx="7.33" ry="7.33" transform="translate(-41.96 60.43) rotate(-19.47)" />
